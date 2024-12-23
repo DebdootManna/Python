f00c18a9a73fbf76f51a3c1217583b6ed9f868a8
Debdoot Manna <106653463+DebdootManna@users.noreply.github.com>
2024-12-23 20:07:50 +0530

    Initial commit

---
+++ 1.py
@@ -0,0 +1,35 @@
+# list = [1, 2, 3, 4, 5]
+# # print(list)
+# # print(list[1:])
+# list.reverse()
+# # print(list)
+# list2 = [6, 7, 8, 9, 10]
+# list.append(list2)
+# print(list)
+# tuple = ["Debdoot", 911, "Aryan", 69]
+# print(tuple)
+# a = (1, 2, 3)
+# b = (4)
+# appendedtable = a + (b,)
+# print(appendedtable)
+# list = []
+# last_timepass = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,]
+# for num in last_timepass:
+#     if num % 2 == 0:
+#         list.append(num)
+# print(list)
+# for num in last_timepass:
+#     if num % 2 == 1:
+#         list.append(num)
+# print(list)
+# numbers = [1,2,3,4,5,6,7,8,9,10]
+# output = [num**2 for num in numbers]
+# print(output)
+x = ("abc","def","ghi")
+p,q,r = x
+print(r)
+# x = ("abc","def","ghi")
+# y = list(x)
+# y = "jkl"
+# x = tuple(y)
+# print(x,y)
